f09fb9d643b40d516f185cbb19ed6ab47cb036d0
Carlo Alberto Ferraris <github@cafxx.strayorange.com>
2018-10-10 10:52:55 +0900

    Update intern.go

--- intern.go
+++ intern.go
@@ -8,37 +8,47 @@ package intern
 
 import "sync"
 
-var (
-	pool sync.Pool = sync.Pool{
-		New: func() interface{} {
-			return make(map[string]string)
-		},
-	}
-)
+var pool = sync.Pool{
+	New: func() interface{} {
+		return make(map[string]string)
+	},
+}
 
 // String returns s, interned.
 func String(s string) string {
 	m := pool.Get().(map[string]string)
-	c, ok := m[s]
-	if ok {
-		pool.Put(m)
-		return c
+	if c, found := m[s]; found {
+		s = c
+	} else {
+		m[s] = s
 	}
-	m[s] = s
 	pool.Put(m)
 	return s
 }
 
-// Bytes returns b converted to a string, interned.
-func Bytes(b []byte) string {
+// Strings returns the strings, interned
+func Strings(ss ...string) []string {
 	m := pool.Get().(map[string]string)
-	c, ok := m[string(b)]
-	if ok {
-		pool.Put(m)
-		return c
+	for i, s := range ss {
+		if c, found := m[s]; found {
+			ss[i] = c
+		} else {
+			m[s] = s
+		}
 	}
-	s := string(b)
-	m[s] = s
+	pool.Put(m)
+	return ss
+}
+
+// Bytes returns b converted to a string, interned.
+func Bytes(b []byte) (s string) {
+	m := pool.Get().(map[string]string)
+	if c, found := m[string(b)]; found {
+		s = c
+	} else {
+		s = string(b)
+		m[s] = s
+	}
 	pool.Put(m)
 	return s
 }
